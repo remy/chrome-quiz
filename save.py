27ab00f82c1b1fa06f9fe30d9d3e9281d09118c5
remy <remy@remysharp.com>
2010-10-28 19:24:13 +0100

    Moved index.html to a python template and now if you visit /quiz it'll change the page so that there's no submission to the spreadsheet. It's a little gnarly in that html and JS and Python is mixed!

--- save.py
+++ save.py
@@ -16,8 +16,11 @@
 #
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
+import os
 import gdata.spreadsheet.text_db
 import urllib
+import urlparse
 import config # contains: email, password, spreadsheet, worksheet
 
 def StringToDictionary(row_data):
@@ -30,26 +33,43 @@ def StringToDictionary(row_data):
 class saveUser(webapp.RequestHandler):
     def post(self):
       self.response.headers['Content-Type'] = 'text/plain'
-      
-      try:
-        gd_client = gdata.spreadsheet.service.SpreadsheetsService()
-        gd_client.email = config.email
-        gd_client.password = config.password
-        gd_client.source = 'chrome-quiz'
-        gd_client.ProgrammaticLogin()
-        entry = gd_client.InsertRow(StringToDictionary(self.request.body), config.spreadsheet, config.worksheet)
-        if isinstance(entry, gdata.spreadsheet.SpreadsheetsList):
-          self.response.out.write('1')
-        else:
+      ref = urlparse.urlparse(self.request.referrer)
+      if ref.path == '/': # only save on a root dir request
+        try:
+          gd_client = gdata.spreadsheet.service.SpreadsheetsService()
+          gd_client.email = config.email
+          gd_client.password = config.password
+          gd_client.source = 'chrome-quiz'
+          gd_client.ProgrammaticLogin()
+          entry = gd_client.InsertRow(StringToDictionary(self.request.body), config.spreadsheet, config.worksheet)
+          if isinstance(entry, gdata.spreadsheet.SpreadsheetsList):
+            self.response.out.write('1')
+          else:
+            self.response.out.write('0')
+        except:
           self.response.out.write('0')
-      except:
-        self.response.out.write('0')
-    def get(self):
+      else:
+        self.response.out.write('1') # fake response
+    def get(self): # ignored
       self.response.headers['Content-Type'] = 'text/html'
-      self.response.out.write('only posts accepted.')      
+      self.response.out.write('1')
 
+class home(webapp.RequestHandler):
+    def get(self):
+      path = os.path.join(os.path.dirname(__file__), 'public/index.html')
+      url = urlparse.urlparse(self.request.url)
+      self.response.out.write(
+        template.render(path, 
+        { 
+          'save': url.path is '/', # true if we're saving to db
+          'url': '' if url.path is '/' else 'quiz' # for the sharing links
+        }
+      ))
+      
 def main():
-    application = webapp.WSGIApplication([('/save', saveUser)], 
+    application = webapp.WSGIApplication([('/save', saveUser),
+                                          ('/', home),
+                                          ('/quiz', home)], 
                                         debug=True)
     util.run_wsgi_app(application)
 
